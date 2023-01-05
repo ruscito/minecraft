934995e234c776f8e9d72ed55b6475a7d1166d4a
ruscito <ruscito@gmail.com>
2023-01-04 20:30:45 -0600

    add validation call back extension

--- src/vulkan.c
+++ src/vulkan.c
@@ -7,6 +7,13 @@
 
 static VkInstance instance;
 
+#ifdef RELEASE
+    static const bool enable_validation_layers = false;
+#else
+    static const bool enable_validation_layers = true;
+#endif
+
+
 bool create_vulkan_instance() {
     VkApplicationInfo appInfo={};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -17,29 +24,49 @@ bool create_vulkan_instance() {
     appInfo.apiVersion = VK_API_VERSION_1_0;
 
     uint32_t count=0;
+    uint32_t aux_count = 0;
+
     const char** glfwExtensions;
+    const char** extensions;
 
     glfwExtensions = glfwGetRequiredInstanceExtensions(&count);
+    aux_count = (enable_validation_layers) ? count + 1: count;
+
+    extensions = malloc(count * sizeof(*extensions));
+    if (extensions == NULL){
+        FATAL("malloc allocation faile");
+        return false;
+    }
+    
+    for(int i=0; i<count; i++) {
+        extensions[i] = glfwExtensions[i];
+    }
+
+    // add only if validation callback is needed
+    if (enable_validation_layers) {
+        extensions[count] = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
+        count = aux_count;
+    }
 
 #if defined(__APPLE__)
     count ++;
-    const char **extensions = malloc(count * sizeof(*extensions));
-    if (extensions==NULL) {
+    const char **apple_extra_extensions = malloc(count * sizeof(*apple_extra_extensions));
+    if (apple_extra_extensions==NULL) {
         FATAL("malloc allocation fail");
         return false;
     }
     for(int i=0; i<count-1; i++) {
-        extensions[i] = glfwExtensions[i];
+        apple_extra_extensions[i] = extensions[i];
     }
-    extensions[count-1] = VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME;
+    apple_extra_extensions[count-1] = VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME;
     INFO("Required extensions:")
     for(int i=0; i<count; i++){
-        INFO("  %s", extensions[i]);
+        INFO("  %s", apple_extra_extensions[i]);
     }
 #else
     INFO("Required extensions:")
     for(int i=0; i<count; i++){
-        INFO("  %s", glfwExtensions[i]);
+        INFO("  %s", extensions[i]);
     }
 #endif
 
@@ -48,17 +75,19 @@ bool create_vulkan_instance() {
     createInfo.pApplicationInfo = &appInfo;
 #if defined(__APPLE__)
     createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
-    createInfo.ppEnabledExtensionNames = extensions;
+    createInfo.ppEnabledExtensionNames = apple_extra_extensions;
 #else
-    createInfo.ppEnabledExtensionNames = glfwExtensions;
+    createInfo.ppEnabledExtensionNames = extensions;
 #endif
     createInfo.enabledExtensionCount = count;
     createInfo.enabledLayerCount = 0;
 
     VkResult result = vkCreateInstance(&createInfo, NULL, &instance);
 #if defined(__APPLE__)
-    free(extensions);
+    free(apple_extra_extensions);
 #endif
+    free(extensions);
+
     if (result != VK_SUCCESS) {
         FATAL("Failed to create instance: %d", result);
         return false;
